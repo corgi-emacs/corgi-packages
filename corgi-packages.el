5c634947281752a348f86eb48dc3e8c134e921bb
Arne Brasseur <arne@arnebrasseur.net>
2020-12-23 15:09:48 +0100

    More packages

--- corgi-packages.el
+++ corgi-packages.el
@@ -1,21 +1,35 @@
-(message "INIT LAMBDAISLAND STRAIGHT")
+(defvar corgi-all-packages
+  '((corgi
+     :type git
+     :repo "/home/arne/github/lambdaisland/corgi")
+
+    (corkey
+     :type git
+     :host github
+     :branch "main"
+     :files ("corkey/corkey.el")
+     :repo "lambdaisland/corgi-packages")
+
+    (clj-ns-name
+     :type git
+     :host github
+     :files ("clj-ns-name/clj-ns-name.el")
+     :repo "plexus/plexmacs")
+
+    (walkclj
+     :type git
+     :host github
+     :files ("walkclj.el")
+     :repo "plexus/walkclj")))
 
 (defun straight-recipes-corgi-packages-list ()
-  (list "corgi" "corkey"))
+  (mapcar #'symbol-name (mapcar #'car corgi-all-packages)))
 
 (defun straight-recipes-corgi-packages-retrieve (package)
-  (pcase package
-    (`corgi
-     '(corgi :type git
-             :repo "/home/arne/github/lambdaisland/corgi"))
-    (`corkey
-     '(corkey
-       :type git
-       :host github
-       :branch "main"
-       :files ("corkey/corkey.el")
-       :repo "lambdaisland/corgi-packages"))
-    (_ nil)))
+  (assoc package corgi-all-packages))
+
+(defun straight-recipes-corgi-packages-version ()
+  1)
 
 (add-to-list #'straight-recipe-repositories 'corgi-packages)
 
