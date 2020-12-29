b3456ffe3559c40949fb760f03745ebf83e1698e
Arne Brasseur <arne@arnebrasseur.net>
2020-12-29 09:49:47 +0100

    Add corgi-clojure, use-package-update-scripts

---
+++ corgi-clojure/corgi-clojure.el
@@ -0,0 +1,138 @@
+;;; corgi-clojure.el --- Clojure configuration for Corgi
+;;
+;; Filename: corgi-clojure.el
+;; Package-Requires: ()
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(use-package clojure-mode
+  :magic ("^#![^\n]*/\\(clj\\|clojure\\|bb\\|lumo\\)" . clojure-mode)
+  :config
+  (setq clojure-toplevel-inside-comment-form t
+        ;; Because of CIDER's insistence to send forms to all linked REPLs, we
+        ;; *have* to be able to switch cljc buffer to clj/cljs mode without
+        ;; cider complaining.
+        clojure-verify-major-mode nil)
+
+  ;; TODO: get this upstream. #_ is not a logical sexp
+  (defun corgi/clojure--looking-at-non-logical-sexp (command)
+    "Return non-nil if text after point is \"non-logical\" sexp.
+\"Non-logical\" sexp are ^metadata and #reader.macros."
+    (comment-normalize-vars)
+    (comment-forward (point-max))
+    (looking-at-p "\\(?:#?\\^\\)\\|#:?:?[[:alpha:]]\\|#_"))
+
+  (advice-add #'clojure--looking-at-non-logical-sexp :around #'corgi/clojure--looking-at-non-logical-sexp))
+
+(use-package cider
+  :after (clojure-mode)
+  :diminish cider-mode
+  :config
+  (setq cider-preferred-build-tool 'clojure-cli
+        ;; make sure we can always debug nrepl issues
+        nrepl-log-messages t)
+
+  ;; TODO: clean this up, submit to upstream where possible
+  ;; More CIDER/clojure-mode stuff
+  ;; - logical-sexp doesn't treat #_ correctly
+
+  ;; New function, should go upstream. Kill all associated REPLs
+  (defun corgi/cider-quit-all ()
+    "Quit all current CIDER REPLs."
+    (interactive)
+    (let ((repls (seq-remove (lambda (r)
+                               (equal r (get-buffer "*babashka-repl*")))
+                             (seq-mapcat #'cdr (sesman-current-sessions 'CIDER)))))
+      (seq-do #'cider--close-connection repls))
+    ;; if there are no more sessions we can kill all ancillary buffers
+    (cider-close-ancillary-buffers)
+    ;; need this to refresh sesman browser
+    (run-hooks 'sesman-post-command-hook))
+
+  ;; When asking for a "matching" REPL (clj/cljs), and no matching REPL is found,
+  ;; return any REPL that is there. This is so that cider-quit can be called
+  ;; repeatedly to close all REPLs in a process. It also means that , s s will go
+  ;; to any REPL if there is one open.
+  (defun corgi/around-cider-current-repl (command &optional type ensure)
+    (let ((repl (or
+                 (if (not type)
+                     (or (funcall command nil)
+                         (funcall command 'any))
+                   (funcall command type))
+                 (get-buffer "*babashka-repl*"))))
+      (if (and ensure (null repl))
+          (cider--no-repls-user-error type)
+        repl)))
+
+  (advice-add #'cider-current-repl :around #'corgi/around-cider-current-repl)
+
+  ;; This essentially redefines cider-repls. The main thing it does is return all
+  ;; REPLs by using sesman-current-sessions (plural) instead of
+  ;; sesman-current-session. It also falls back to the babashka repl if no repls
+  ;; are connected/linked, so we can always eval.
+  (defun corgi/around-cider-repls (command &optional type ensure)
+    (let ((type (cond
+                 ((listp type)
+                  (mapcar #'cider-maybe-intern type))
+                 ((cider-maybe-intern type))))
+          (repls (delete-dups (seq-mapcat #'cdr (or (sesman-current-sessions 'CIDER)
+                                                    (when ensure
+                                                      (user-error "No linked %s sessions" system)))))))
+      (or (seq-filter (lambda (b)
+                        (and (cider--match-repl-type type b)
+                             (not (equal b (get-buffer "*babashka-repl*")))))
+                      repls)
+          (list (get-buffer "*babashka-repl*")))))
+
+  (advice-add #'cider-repls :around #'corgi/around-cider-repls)
+
+  (defun corgi/cider-eval-last-sexp-and-replace ()
+    "Alternative to cider-eval-last-sexp-and-replace, but kills
+clojure logical sexp instead of ELisp sexp, and pprints the
+result."
+    (interactive)
+    (let ((last-sexp (cider-last-sexp)))
+      ;; we have to be sure the evaluation won't result in an error
+      (cider-nrepl-sync-request:eval last-sexp)
+      ;; seems like the sexp is valid, so we can safely kill it
+      (let ((opoint (point)))
+        (clojure-backward-logical-sexp)
+        (kill-region (point) opoint))
+      (cider-interactive-eval last-sexp
+                              (cider-eval-pprint-with-multiline-comment-handler
+                               (current-buffer)
+                               (set-marker (make-marker) (point))
+                               ""
+                               " "
+                               "")
+                              nil
+                              (cider--nrepl-print-request-map fill-column))))
+
+  (defun corgi/cider-pprint-eval-last-sexp-insert ()
+    (interactive)
+    (let ((cider-comment-prefix "")
+          (cider-comment-continued-prefix " ")
+          (cider-comment-postfix ""))
+      (cider-pprint-eval-last-sexp-to-comment))))
+
+(use-package clj-refactor
+  :after (cider)
+  :diminish clj-refactor-mode
+  :config
+  (setq cljr-cljc-clojure-test-declaration "[clojure.test :refer [deftest testing is are use-fixtures run-tests join-fixtures]]"
+        cljr-cljs-clojure-test-declaration "[clojure.test :refer [deftest testing is are use-fixtures run-tests join-fixtures]]"
+        cljr-clojure-test-declaration "[clojure.test :refer [deftest testing is are use-fixtures run-tests join-fixtures]]"
+        cljr-eagerly-build-asts-on-startup nil
+        cljr-warn-on-eval nil)
+  :hook ((clojurex-mode-hook
+          clojurescript-mode-hook
+          clojurec-mode-hook
+          clojure-mode-hook)
+         . clj-refactor-mode))
+
+(provide 'corgi-clojure)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; corgi-clojure.el ends here
