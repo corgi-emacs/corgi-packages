485f63094f2c335bf8ce05dc6b274f80b68e2ae7
Arne Brasseur <arne@arnebrasseur.net>
2021-01-01 10:56:54 +0100

    Add xclip

--- corgi-editor/corgi-editor.el
+++ corgi-editor/corgi-editor.el
@@ -131,6 +131,11 @@
 
 (use-package string-edit)
 
+(use-package xclip
+  :config
+  (unless (executable-find xclip-program)
+    (xclip-mode t)))
+
 ;; Offer to create parent directories if they do not exist
 ;; http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
 (defun magnars/create-non-existent-directory ()
