a9bdcf5fe7878e2be1076d0b68cd7c7dfd5d9a0b
Arne Brasseur <arne@arnebrasseur.net>
2021-12-31 15:12:10 -0500

    Prevent interdependency

--- corgi-commands/corgi-commands.el
+++ corgi-commands/corgi-commands.el
@@ -45,35 +45,44 @@ Repeated invocations toggle between the two most recently open buffers."
   (interactive)
   (find-file (expand-file-name "init.el" user-emacs-directory)))
 
+(defun corgi/-locate-file (fname)
+  "Like corkey/-locate-file"
+  (cond
+   ((symbolp fname)
+    (corgi/-locate-file (concat (symbol-name fname) ".el")))
+   ((file-exists-p (expand-file-name fname user-emacs-directory))
+    (expand-file-name fname user-emacs-directory))
+   (t (locate-library fname))))
+
 (defun corgi/open-keys-file ()
   "Open the Corgi built-in key binding file"
   (interactive)
-  (find-file (corkey/-locate-file 'corgi-keys)))
+  (find-file (corgi/-locate-file 'corgi-keys)))
 
 (defun corgi/open-signals-file ()
   "Open the Corgi built-in signals file"
   (interactive)
-  (find-file (corkey/-locate-file 'corgi-signals)))
+  (find-file (corgi/-locate-file 'corgi-signals)))
 
 (defun corgi/open-user-keys-file ()
   "Open the user key binding file in the emacs user directory
 Will create one if it doesn't exist."
   (interactive)
-  (let ((keys-file (corkey/-locate-file 'user-keys)))
+  (let ((keys-file (corgi/-locate-file 'user-keys)))
     (when (not keys-file)
-      (copy-file (corkey/-locate-file 'user-keys-template)
+      (copy-file (corgi/-locate-file 'user-keys-template)
                  (expand-file-name "user-keys.el" user-emacs-directory)))
-    (find-file (corkey/-locate-file 'user-keys))))
+    (find-file (corgi/-locate-file 'user-keys))))
 
 (defun corgi/open-user-signals-file ()
   "Open the user signal file in the emacs user directory
 Will create one if it doesn't exist."
   (interactive)
-  (let ((signals-file (corkey/-locate-file 'user-signals)))
+  (let ((signals-file (corgi/-locate-file 'user-signals)))
     (when (not signals-file)
-      (copy-file (corkey/-locate-file 'user-signals-template)
+      (copy-file (corgi/-locate-file 'user-signals-template)
                  (expand-file-name "user-signals.el" user-emacs-directory)))
-    (find-file (corkey/-locate-file 'user-signals))))
+    (find-file (corgi/-locate-file 'user-signals))))
 
 ;; Taking this out, see explanation in corgi-keys.el
 ;;
