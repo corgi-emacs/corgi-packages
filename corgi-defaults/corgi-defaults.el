6ff67964c9cb5477e35b308d09ce9d9bf8f1cdd0
Arne Brasseur <arne@arnebrasseur.net>
2026-09-11 16:00:44 +0200

    Modernise corgi-defaults, minimum version is now Emacs 29.1

--- corgi-defaults/corgi-defaults.el
+++ corgi-defaults/corgi-defaults.el
@@ -14,21 +14,17 @@
 ;;; Code:
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
-(setq inhibit-startup-message t)
-
-;; Allow pasting selection outside of Emacs
-(setq select-enable-clipboard t)
+(setq inhibit-startup-screen t)
 
 ;; Auto refresh buffers
 ;; Also auto refresh dired, but be quiet about it
 (require 'autorevert)
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-(add-hook 'dired-mode-hook #'auto-revert-mode)
+(setq global-auto-revert-non-file-buffers t
+      auto-revert-verbose nil)
 (global-auto-revert-mode 1)
 
 ;; Show keystrokes in progress
@@ -37,44 +33,26 @@
 ;; Move files to trash when deleting
 (setq delete-by-moving-to-trash t)
 
-;; Real emacs knights don't use shift to mark things
+;; Don't use shift to mark things. This is Emacs, not Notepad.
 (setq shift-select-mode nil)
 
-;; Transparently open compressed files
-(auto-compression-mode t)
-
-;; Enable syntax highlighting for older Emacsen that have it off
-(global-font-lock-mode t)
-
-;; Answering just 'y' or 'n' will do
-(defalias 'yes-or-no-p 'y-or-n-p)
+;; Answering just 'y' or 'n' will do. Sharp tools for sharp minds.
+(setq use-short-answers t)
 
 ;; UTF-8 please
-(setq locale-coding-system 'utf-8)   ; pretty
-(set-terminal-coding-system 'utf-8)  ; pretty
-(set-keyboard-coding-system 'utf-8)  ; pretty
-(set-selection-coding-system 'utf-8) ; please
-(prefer-coding-system 'utf-8)        ; with sugar on top
-
-;; Show active region
-(transient-mark-mode 1)
-(make-variable-buffer-local 'transient-mark-mode)
-(put 'transient-mark-mode 'permanent-local t)
-(setq-default transient-mark-mode t)
+(prefer-coding-system 'utf-8)
 
 ;; Always display line and column numbers
-(setq line-number-mode t)
-(setq column-number-mode t)
+(column-number-mode 1)
 
 ;; Lines should be 80 characters wide, not 72
-(setq fill-column 80)
-(set-default 'fill-column 80)
+(setq-default fill-column 80)
 
 ;; Never insert tabs
-(set-default 'indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
 
 ;; Show me empty lines after buffer end
-(set-default 'indicate-empty-lines t)
+(setq-default indicate-empty-lines t)
 
 ;; Easily navigate sillycased words
 (global-subword-mode 1)
@@ -89,34 +67,26 @@
 (setq gc-cons-threshold 2000000)
 
 ;; Sentences do not need double spaces to end. Period.
-(set-default 'sentence-end-double-space nil)
+(setq-default sentence-end-double-space nil)
 
 ;; Add parts of each file's directory to the buffer name if not unique
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
-;; No electric indent
-(setq electric-indent-mode nil)
-
-;; Nic says eval-expression-print-level needs to be set to nil (turned off) so
-;; that you can always see what's happening.
-(setq eval-expression-print-level nil)
-
-;; Don't make backup~ files
-;; (setq make-backup-files nil)
-
-(when (not (file-directory-p (expand-file-name "backups" user-emacs-directory)))
-  (make-directory (expand-file-name "backups" user-emacs-directory)))
-
-(when (not (file-directory-p (expand-file-name "auto-save-list" user-emacs-directory)))
-  (make-directory (expand-file-name "auto-save-list" user-emacs-directory)))
+;; Default is 4 / 12 , show a generous amount in the echo area, but do have a safety limit
+(setq eval-expression-print-level 6
+      eval-expression-print-length 300)
 
 ;; Put backups and auto-save files in subdirectories, so the
 ;; user-emacs-directory doesn't clutter
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name "backups" user-emacs-directory)))
-      auto-save-file-name-transforms
-      `((".*" ,(expand-file-name "auto-save-list/" user-emacs-directory) t)))
+(let ((backup-dir (expand-file-name "backups" user-emacs-directory))
+      (auto-save-dir (expand-file-name "auto-save-list/" user-emacs-directory))
+      (tramp-auto-save-dir (expand-file-name "auto-save-list/tramp/" user-emacs-directory)))
+  (make-directory backup-dir t)
+  (make-directory auto-save-dir t)
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-save-dir t))
+        tramp-auto-save-directory tramp-auto-save-dir))
 
 ;; Stop asking about following symlinks to version controlled files
 (setq vc-follow-symlinks t)
@@ -124,18 +94,18 @@
 ;; When Emacs is ran in GUI mode, configure common Emoji fonts, making it more
 ;; likely that Emoji will work out of the box
 (when (display-graphic-p)
-  (set-fontset-font t 'symbol "Apple Color Emoji")
-  (set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
-  (set-fontset-font t 'symbol "Segoe UI Emoji" nil 'append)
-  (set-fontset-font t 'symbol "Symbola" nil 'append))
+  (set-fontset-font t 'emoji "Apple Color Emoji" nil 'append)
+  (set-fontset-font t 'emoji "Noto Color Emoji" nil 'append)
+  (set-fontset-font t 'emoji "Segoe UI Emoji" nil 'append)
+  (set-fontset-font t 'emoji "Twemoji Mozilla" nil 'append)
+  (set-fontset-font t 'emoji "Joypixels" nil 'append)
+  (set-fontset-font t 'emoji "Symbola" nil 'append))
 
-(setq ring-bell-function 'ignore)
+(setq ring-bell-function #'flash-face-bell-function)
 
-;; Configure mac modifiers to be what you expect, and turn off the bell noise
-(when (equal system-type 'darwin)
-  (with-no-warnings
-    (setq mac-command-modifier 'control
-          mac-option-modifier 'meta)))
+;; Configure mac modifiers to be what you expect -> Cmd = Control (e.g. type Cmd-q for C-q)
+(when (eq system-type 'darwin)
+  (setopt ns-command-modifier 'control))
 
 (provide 'corgi-defaults)
 
